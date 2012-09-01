8a27889d070cbd18b18008215bc04664acb903eb
Adam Price <komidore64@gmail.com>
2012-08-31 23:46:47 -0400

    problem #5

--- ruby/one_to_ten/five.rb
+++ ruby/one_to_ten/five.rb
@@ -5,3 +5,27 @@ the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly
 divisible by all of the numbers from 1 to 20?
 =end
+
+@divs = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+def is_twenty_div(num)
+  @divs.each do |d|
+    return false if (num % d).zero?
+  end
+  return true
+end
+
+num = 2
+while not is_twenty_div(num)
+  num += 2
+end
+
+puts num
+
+=begin
+this one is my favorite but i don't understand how it works
+
+require 'rational'
+num = (1..20).inject(1) { |result, n| result.lcm n }
+
+=end
