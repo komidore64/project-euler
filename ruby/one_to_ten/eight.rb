2bfb4e7883024dc4fc4158f1dce0ee6737ce70d2
Adam Price <komidore64@gmail.com>
2012-09-05 23:34:23 -0400

    solved problem #8

---
+++ ruby/one_to_ten/eight.rb
@@ -0,0 +1,19 @@
+=begin
+Find the greatest product of five consecutive digits in the 1000-digit number.
+=end
+
+num_str = File.open("eight.text", "r").read.delete("\n")
+
+digits = num_str.length - 5
+largest_product = 0
+
+digits.times do |i|
+  temp_prod = num_str[i].chr.to_i
+  temp_prod *= num_str[i + 1].chr.to_i
+  temp_prod *= num_str[i + 2].chr.to_i
+  temp_prod *= num_str[i + 3].chr.to_i
+  temp_prod *= num_str[i + 4].chr.to_i
+  largest_product = temp_prod if temp_prod > largest_product
+end
+
+puts largest_product
