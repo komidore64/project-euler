e0c4d2a8a3e8b5a1ee319016102fce318fe22d17
Adam Price <komidore64@gmail.com>
2012-09-16 20:28:31 -0400

    problem #16 solved

---
+++ ruby/eleven_to_twenty/sixteen.rb
@@ -0,0 +1,13 @@
+#
+# 2 ^ 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 2 ^ 1000?
+#
+
+puts (2 ** 1000).to_s.each_char.inject(0) { |sum, c| sum + c.to_i }
+
+# time ruby sixteen.rb
+#
+# real  0m0.014s
+# user  0m0.007s
+# sys   0m0.005s
