be5ae9381a05da85db7afde619fb36ea5d701f8b
Adam Price <komidore64@gmail.com>
2012-09-17 22:39:16 -0400

    still working on #17

--- ruby/eleven_to_twenty/seventeen.rb
+++ ruby/eleven_to_twenty/seventeen.rb
@@ -11,23 +11,135 @@
 # contains 20 letters. The use of "and" when writing out numbers is
 # in compliance with British usage.
 
+def get_ten_to_nineteen(num)
+  case num
+  when 10
+    return "ten"
+  when 11
+    return "eleven"
+  when 12
+    return "twelve"
+  when 13
+    return "thirteen"
+  when 14
+    return "fourteen"
+  when 15
+    return "fifteen"
+  when 16
+    return "sixteen"
+  when 17
+    return "seventeen"
+  when 18
+    return "eighteen"
+  when 19
+    return "nineteen"
+  else
+    raise "oh snap"
+  end
+end
+
 def get_ones_digit(num)
   case num
+  when 0
+    return ""
   when 1
-    return "one".length
+    return "one"
   when 2
-    return "two".length
+    return "two"
   when 3
-    return "three".length
+    return "three"
   when 4
-    return "four".length
+    return "four"
   when 5
-    return "five".length
+    return "five"
   when 6
-    return "six".length
+    return "six"
   when 7
-    return "seven".length
+    return "seven"
   when 8
-    return "eight".length
+    return "eight"
   when 9
-    return "nine".length
+    return "nine"
+  else
+    raise "oh noes"
+  end
+end
+
+def get_tens_digit(num)
+  case num
+  when 0
+    return ""
+  when 1
+    raise "shouldn't hit 10"
+  when 2
+    return "twenty"
+  when 3
+    return "thirty"
+  when 4
+    return "fourty"
+  when 5
+    return "fifty"
+  when 6
+    return "sixty"
+  when 7
+    return "seventy"
+  when 8
+    return "eighty"
+  when 9
+    return "ninety"
+  else
+    raise "something died"
+  end
+end
+
+def get_hundred_digit(num)
+  case num
+  when 0
+    return ""
+  when 1
+    return "one hundred and"
+  when 2
+    return "two hundred and"
+  when 3
+    return "three hundred and"
+  when 4
+    return "four hundred and"
+  when 5
+    return "five hundred and"
+  when 6
+    return "six hundred and"
+  when 7
+    return "seven hundred and"
+  when 8
+    return "eight hundred and"
+  when 9
+    return "nine hundred and"
+  else
+    raise "shouldn't be here"
+  end
+end
+
+def get_thousand_digit(num)
+  case num
+  when 0
+    return ""
+  when 1
+    return "one thousand"
+  else
+    raise "busted"
+  end
+end
+
+
+total_words = ""
+
+(1..100).each do |n|
+  str = "%04d" % n
+  if n > 9 and n < 20
+    total_words << get_ten_to_nineteen(n)
+  else
+    total_words << get_thousand_digit(n[0]) + get_hundred_digit(n[1]) + get_tens_digit(n[2]) + get_ones_digit(n[3]) # fail
+  end
+end
+
+puts total_words.delete(" ").length
