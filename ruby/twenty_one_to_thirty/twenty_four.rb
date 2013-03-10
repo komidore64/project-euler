e45ca5e39b347a34b7a285534b374935add1ef01
Adam Price <komidore64@gmail.com>
2013-03-09 21:47:24 -0500

    solving number 24

---
+++ ruby/twenty_one_to_thirty/twenty_four.rb
@@ -0,0 +1,21 @@
+#
+# A permutation is an ordered arrangement of objects. For example, 3124 is one
+# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+# are listed numerically or alphabetically, we call it lexicographic order. The
+# lexicographic permutations of 0, 1 and 2 are:
+#
+# 012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
+# 5, 6, 7, 8 and 9?
+#
+
+puts [0,1,2,3,4,5,6,7,8,9].permutation.to_a.collect! { |arr| arr.inject("") { |num, c| num << c.to_s }.to_i }[999999]
+
+# time ruby twenty_four.rb
+#
+# real    0m13.831s
+# user    0m13.582s
+# sys     0m0.202s
+#
+# It feels a little bit like cheating just using Array#permutation...
