81334b113bf3e413d8f88b3db0f532b412eff99e
Adam Price <komidore64@gmail.com>
2013-03-29 10:06:41 -0400

    formatting

--- ruby/eleven_to_twenty/thirteen.rb
+++ ruby/eleven_to_twenty/thirteen.rb
@@ -3,7 +3,7 @@
 # these one-hundred 50-digit numbers.
 #
 
-puts File.open("thirteen.text").read.split.collect {|n| n.to_i}.inject(:+).to_s[0..9]
+puts File.open("thirteen.text").read.split.collect { |n| n.to_i }.inject(:+).to_s[0..9]
 
 # time ruby thirteen.rb
 #
