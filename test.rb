d1b5fb9ca425865b5a624b5b93874d570966d148
Adam Price <komidore64@gmail.com>
2019-12-21 20:02:06 -0500

    problem 15

--- test.rb
+++ test.rb
@@ -70,4 +70,9 @@ class ProjectEuler < MiniTest::Test
     result = %x{ruby p0014.rb}
     assert_equal result.strip.to_i, 837799
   end
+
+  def test_0015
+    result = %x{ruby p0015.rb}
+    assert_equal result.strip.to_i, 137846528820
+  end
 end
